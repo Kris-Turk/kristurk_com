ec84f853552a6bfb7a6ab5373725aaf32258c091
Kris Turk <kris.turk@gitch.solutions>
2024-11-18 09:51:34 +1300

    webform

--- components/navbar.js
+++ components/navbar.js
@@ -1,24 +1,16 @@
 function loadNavbar() {
-    // Check if we're on the YouTube page
-    const isYoutubePage = window.location.pathname.includes('youtube');    
-
-
     const navbar = `
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Kris Turk</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="./index.html">Kris Turk</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="youtube.html">YouTube</a>
-                        </li>
-                        ${blogLink}
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="./index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./youtube.html">YouTube</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./contact.html">Contact</a></li>
                     </ul>
                 </div>
             </div>
