aad4d6cf8f372c734f9be4bc9236b43e5db01cbc
Kris Turk <kris.turk@gitch.solutions>
2024-11-17 22:51:18 +1300

    updated api key funciton

--- api/config/index.js
+++ api/config/index.js
@@ -1,1 +1,11 @@
- 
+module.exports = async function (context, req) {
+    context.res = {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            youtubeApiKey: process.env.YOUTUBE_API_KEY,
+            youtubeChannelId: process.env.YOUTUBE_CHANNEL_ID
+        }
+    };
+}; 
