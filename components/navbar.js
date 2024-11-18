977bb6a5936a68f6550da39b8f1fb0a5e9442198
Kris Turk <kris.turk@gitch.solutions>
2024-11-18 20:46:59 +1300

    mobile nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -10,7 +10,7 @@ function loadNavbar() {
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="./index.html">Kris Turk</a>
-                <button class="navbar-toggler navbar-light bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #ff8c00;">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
