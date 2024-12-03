440cdf59b1e3be5b27baa5c8dde39b918e9142f8
Kris Turk <kris.turk@gitch.solutions>
2024-12-03 21:47:01 +1300

    added blog

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,48 @@
 function loadNavbar() {
     console.log('Navbar component is loading...');
     
-    // Detect mobile OS
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    const isAndroid = /Android/.test(navigator.userAgent);
-    console.log('Device detection:', { isIOS, isAndroid });
-
+    // Add CSS for hover functionality
+    const style = document.createElement('style');
+    style.textContent = `
+        @media (min-width: 992px) {
+            .dropdown:hover .dropdown-menu {
+                display: block;
+            }
+            .dropdown-menu {
+                margin-top: 0;
+            }
+        }
+    `;
+    document.head.appendChild(style);
+    
+    // Determine if we're in a subdirectory
+    const isInBlogPosts = window.location.pathname.includes('blog-posts');
+    const prefix = isInBlogPosts ? '../' : './';
+    
     const navbar = `
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="./index.html">Kris Turk</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #F08013;">
+                <a class="navbar-brand" href="${prefix}index.html">Kris Turk</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #ff8c00;">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="./index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="./youtube.html">YouTube</a></li>
-                        <li class="nav-item"><a class="nav-link" href="./contact.html">Contact</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="${prefix}index.html">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="resourcesDropdown" role="button" aria-expanded="false">
+                                Resources
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="resourcesDropdown">
+                                <li><a class="dropdown-item" href="${prefix}youtube.html">YouTube</a></li>
+                                <li><a class="dropdown-item" href="${prefix}blog.html">Blog</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="${prefix}contact.html">Contact</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -28,51 +53,6 @@ function loadNavbar() {
     if (navbarPlaceholder) {
         console.log('Found navbar placeholder, inserting navbar...');
         navbarPlaceholder.innerHTML = navbar;
-
-        const navbarToggler = document.querySelector('.navbar-toggler');
-        const navbarCollapse = document.querySelector('.navbar-collapse');
-        
-        if (navbarToggler && navbarCollapse) {
-            if (isIOS) {
-                // iOS-specific behavior
-                console.log('Using iOS navigation behavior');
-                navbarToggler.addEventListener('click', () => {
-                    navbarCollapse.classList.toggle('show');
-                });
-
-                document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-                    link.addEventListener('click', () => {
-                        navbarCollapse.classList.remove('show');
-                    });
-                });
-            } else if (isAndroid) {
-                // Android-specific behavior
-                console.log('Using Android navigation behavior');
-                const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-                    toggle: false
-                });
-
-                navbarToggler.addEventListener('click', () => {
-                    bsCollapse.toggle();
-                });
-
-                document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-                    link.addEventListener('click', () => {
-                        bsCollapse.hide();
-                    });
-                });
-            } else {
-                // Desktop behavior
-                console.log('Using desktop navigation behavior');
-                const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-                    toggle: false
-                });
-
-                navbarToggler.addEventListener('click', () => {
-                    bsCollapse.toggle();
-                });
-            }
-        }
     } else {
         console.error('Could not find navbar-placeholder element!');
     }
